4f8981289ad640406ac81ef9be40e920ab972949
fran <francisco.vento@outlook.com>
2023-05-15 01:36:46 -0300

    feat: layout icon push to base route

--- components/navigation/AppHeader.tsx
+++ components/navigation/AppHeader.tsx
@@ -1,17 +1,20 @@
 import { logout } from '@/lib/helpers/logout.helper';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import ResimexIcon from '../svg/ResimexIcon';
 
 interface IAppHeader {
   bgTransparent?: boolean;
 }
 const AppHeader = ({ bgTransparent = false }: IAppHeader) => {
+  const router = useRouter();
+
   return (
     <div
       className={`header-diagonal  py-4 ${bgTransparent && '!bg-transparent'}`}
     >
       <div className="px-12 py-2 diagonal flex items-center justify-between">
-        <Link href={'/dashboard'}>
+        <Link href={router.pathname.includes('auth') ? '/' : '/dashboard'}>
           <ResimexIcon />
         </Link>
         <div className="underline cursor-pointer" onClick={logout}>
